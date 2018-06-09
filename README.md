9c0561353a4b08c22a32cc8d8f3b8a38d28c649c
MacBookPro <MacBookPro@MacBookPros-MacBook-Po.local>
2018-06-09 06:18:50 +0300

    update the arcade game

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-frontend-nanodegree-arcade-game
+## Description of Classic Aracade Game
+The front end of udacity nanodegree, I written a code for classic arcade game based on the game frogger. The point of the game is to get your character from the grass over the road and into the water. The challenge is to avoid the bugs **Enemy**  that walking through the road. If a bug **Enemy** is touched, the character is reset back at the beginning. The user is tring to reaching the water, if the user reached will rest back to the beginning.
+
+
+## Installing
+Please following these steps
+1.  Open your terminal post:
+    `git clone my*********`
+2.  Open index.html in your browser.
+
+3.  Play the game and enjoying.
+
+
+## How to play:
+The goal is to reach to the water.
+
+1.  Using the arrow keys on your Keyboard, move upwards.
+
+2.  Try and avoid the cockroaches and try and jamp into the water.
+
+3. if you made it in the water, you can play again. If you collided with a cockrach, start again!
+
+## Used:
+    *   Getting started Udacity exersize:
+    [UdacityProject](https://github.com/udacity/frontend-nanodegree-arcade-game)
+
+
+
 ===============================
 
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,31 +16,114 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    
+    // One enenmies are off the canvas , they apeappear tandomly 
+    // With different speeds
+    if (this.x > 510){
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 500);
+    };
+    
+    //See if coolisions bitween the player and enemies
+    if (player.x < this.x + 80 &&
+        player.x + 80 > this.x &&
+        player.y < this.y + 60 &&
+        60 + player.y > this.y){
+        player.x = 200;
+        player.y = 400;
+     
+    };
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+
+// Now write your own player class
+var Player = function (x, y) {
+    //Variables for the player to move x and y axis
+    this.x = x;
+    this.y = y;
+    // The character of the player
+    this.player = 'images/char-horn-girl.png';
+};
+
+// This  an update() of class Player 
+Player.prototype.update = function(dt) {
+    
+};
+
+// This one for renders the image of the user into game, render() and
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
+
+
+// a handleInput() method.
+Player.prototype.handleInput = function (keyPress) {
+
+    // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
+    if (keyPress == 'lt' && this.x > 0) {
+        this.x -= 102;
+    };
+
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
+    if (keyPress == 'rt' && this.x < 400) {
+        this.x += 100;
+    };
+
+    // Enables user on up arrow key to move upwards on the y axis by 83
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 85;
+    };
+
+    // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
+    if (keyPress == 'down' && this.y < 400) {
+        this.y += 85;
+    };
+
+    // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position
+    if (this.y < 0) {
+        setTimeout(() => {
+            this.x = 200;
+            this.y = 400;
+        }, 500);
+    };
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Place all enemy objects in an array called allEnemie
+var allEnemies = [];
 
+var enemyLocation = [60,150,220]; // location of the three enemies on y axis
+
+
+//All Char of player
+var allCharPlayer = ['char-boy.png','char-cat-girl.png','char-horn-girl.png','char-pink-girl.png','char-princess-girl.png'];
+enemyLocation.forEach(function(location_Y) {
+   enemyObj = new Enemy(0, location_Y, 200);
+    allEnemies.push(enemyObj);
+});
+
+// Place the player object in a variable called player
+var player = new Player(200,400);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
+        37: 'lt',
         38: 'up',
-        39: 'right',
+        39: 'rt',
         40: 'down'
     };
 
